030531bbda0a9734dbed17871925d0b941fa2ec4
tomcr00ze <tomcroose101@gmail.com>
2024-04-07 22:18:10 +0530

    example from typescript doc. added types.

---
+++ intro.ts
@@ -0,0 +1,10 @@
+// Writing Typescript code ==> Javascript with types 
+interface User {
+    name: string;
+    id: number;
+}
+const user: User = {
+    name: "Hayes",
+    id: 0,
+};
+console.log(user.name);
