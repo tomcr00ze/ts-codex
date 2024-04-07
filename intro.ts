64534a87e2d24270740f0fff92b8b3c226742a17
tomcr00ze <tomcroose101@gmail.com>
2024-04-07 22:20:35 +0530

    transpiled ts to js code using tsc into.ts command

--- intro.ts
+++ intro.ts
@@ -3,8 +3,16 @@ interface User {
     name: string;
     id: number;
 }
+
 const user: User = {
     name: "Hayes",
     id: 0,
 };
 console.log(user.name);
+
+// following is the generated javascript code --:
+// var user = {
+//     name: "Hayes",
+//     id: 0,
+// };
+// console.log(user.name);
